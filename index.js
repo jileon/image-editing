39119edd082daf8c342d037bcf6e368efdb1c21d
Janet Leon <ajleon00@gmail.com>
2019-02-11 22:11:59 -0800

    adds event listeners to inputs for changes

---
+++ index.js
@@ -0,0 +1,7 @@
+'use-strict';
+
+const inputs= document.querySelectorAll('.controls input');
+function handleUpdate(){
+  console.log(this.value);
+}
+inputs.forEach(item=>item.addEventListener('change', handleUpdate));
